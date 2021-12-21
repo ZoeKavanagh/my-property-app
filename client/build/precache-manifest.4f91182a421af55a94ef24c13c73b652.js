19466f50b84b097dab82b5559675a2614fa40092
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2021-12-21 18:46:58 +0000

    Add .gitignore to root folder

---
+++ client/build/precache-manifest.4f91182a421af55a94ef24c13c73b652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ea2965642bf5deb8a2",
+    "url": "/static/css/main.3397bdaa.chunk.css"
+  },
+  {
+    "revision": "31ea2965642bf5deb8a2",
+    "url": "/static/js/main.31ea2965.chunk.js"
+  },
+  {
+    "revision": "a230346c0ef1f8dc478e",
+    "url": "/static/js/1.a230346c.chunk.js"
+  },
+  {
+    "revision": "38bdf0906a4ebfb62da6",
+    "url": "/static/css/2.ccaa6a00.chunk.css"
+  },
+  {
+    "revision": "38bdf0906a4ebfb62da6",
+    "url": "/static/js/2.38bdf090.chunk.js"
+  },
+  {
+    "revision": "00b99ab3cb25e75f6098",
+    "url": "/static/js/runtime~main.00b99ab3.js"
+  },
+  {
+    "revision": "dd5191889f6b4f0dae4b3859f455acd0",
+    "url": "/index.html"
+  }
+];
